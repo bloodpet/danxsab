ec77d33eb03234341af7cb6d6c1b451504b91588
Eman Calso <egcalso@gmail.com>
2018-10-14 23:15:03 +0800

    Fix navigation on mobile
    Hide navbar when nav-link is clicked

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -89,8 +89,122 @@ $(document).ready(function(){
 
 });
 
+pk = {
+  misc:{
+      navbar_menu_visible: 0
+  },
 
-$(document).on('click', '.navbar-toggler', function(){
+  checkScrollForPresentationPage: debounce(function(){
+      oVal = ($(window).scrollTop() / 3);
+      big_image.css({
+          'transform':'translate3d(0,' + oVal +'px,0)',
+          '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
+          '-ms-transform':'translate3d(0,' + oVal +'px,0)',
+          '-o-transform':'translate3d(0,' + oVal +'px,0)'
+      });
+  }, 4),
+
+  checkScrollForTransparentNavbar: debounce(function() {
+        if($(document).scrollTop() > $(".navbar").attr("color-on-scroll") ) {
+              if(transparent) {
+                  transparent = false;
+                  $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+              }
+          } else {
+              if( !transparent ) {
+                  transparent = true;
+                  $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+              }
+          }
+  }, 17),
+
+  initPopovers: function(){
+      if($('[data-toggle="popover"]').length != 0){
+          $('body').append('<div class="popover-filter"></div>');
+
+          //    Activate Popovers
+          $('[data-toggle="popover"]').popover().on('show.bs.popover', function () {
+              $('.popover-filter').click(function(){
+                  $(this).removeClass('in');
+                  $('[data-toggle="popover"]').popover('hide');
+              });
+              $('.popover-filter').addClass('in');
+          }).on('hide.bs.popover', function(){
+              $('.popover-filter').removeClass('in');
+          });
+
+      }
+  },
+  initCollapseArea: function(){
+      $('[data-toggle="pk-collapse"]').each(function () {
+          var thisdiv = $(this).attr("data-target");
+          $(thisdiv).addClass("pk-collapse");
+      });
+
+      $('[data-toggle="pk-collapse"]').hover(function(){
+          var thisdiv = $(this).attr("data-target");
+          if(!$(this).hasClass('state-open')){
+              $(this).addClass('state-hover');
+              $(thisdiv).css({
+                  'height':'30px'
+              });
+          }
+
+      },
+      function(){
+          var thisdiv = $(this).attr("data-target");
+          $(this).removeClass('state-hover');
+
+          if(!$(this).hasClass('state-open')){
+              $(thisdiv).css({
+                  'height':'0px'
+              });
+          }
+      }).click(function(event){
+              event.preventDefault();
+
+              var thisdiv = $(this).attr("data-target");
+              var height = $(thisdiv).children('.panel-body').height();
+
+              if($(this).hasClass('state-open')){
+                  $(thisdiv).css({
+                      'height':'0px',
+                  });
+                  $(this).removeClass('state-open');
+              } else {
+                  $(thisdiv).css({
+                      'height':height + 30,
+                  });
+                  $(this).addClass('state-open');
+              }
+          });
+  },
+  initSliders: function(){
+      // Sliders for demo purpose in refine cards section
+      if($('#sliderRegular').length != 0 ){
+          var rangeSlider = document.getElementById('sliderRegular');
+          noUiSlider.create(rangeSlider, {
+            start: [ 5000 ],
+            range: {
+              'min': [  2000 ],
+              'max': [ 10000 ]
+            }
+          });
+      }
+      if($('#sliderDouble').length != 0){
+          var slider = document.getElementById('sliderDouble');
+          noUiSlider.create(slider, {
+            start: [20, 80],
+            connect: true,
+            range: {
+              'min': 0,
+              'max': 100
+            }
+          });
+      }
+  },
+
+  toggleNavBar: function(){
     $toggle = $(this);
     if(pk.misc.navbar_menu_visible == 1) {
         $('html').removeClass('nav-open');
@@ -117,122 +231,7 @@ $(document).on('click', '.navbar-toggler', function(){
         $('html').addClass('nav-open');
         pk.misc.navbar_menu_visible = 1;
     }
-});
-
-pk = {
-    misc:{
-        navbar_menu_visible: 0
-    },
-
-    checkScrollForPresentationPage: debounce(function(){
-        oVal = ($(window).scrollTop() / 3);
-        big_image.css({
-            'transform':'translate3d(0,' + oVal +'px,0)',
-            '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
-            '-ms-transform':'translate3d(0,' + oVal +'px,0)',
-            '-o-transform':'translate3d(0,' + oVal +'px,0)'
-        });
-    }, 4),
-
-    checkScrollForTransparentNavbar: debounce(function() {
-        	if($(document).scrollTop() > $(".navbar").attr("color-on-scroll") ) {
-                if(transparent) {
-                    transparent = false;
-                    $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
-                }
-            } else {
-                if( !transparent ) {
-                    transparent = true;
-                    $('.navbar[color-on-scroll]').addClass('navbar-transparent');
-                }
-            }
-    }, 17),
-
-    initPopovers: function(){
-        if($('[data-toggle="popover"]').length != 0){
-            $('body').append('<div class="popover-filter"></div>');
-
-            //    Activate Popovers
-           $('[data-toggle="popover"]').popover().on('show.bs.popover', function () {
-                $('.popover-filter').click(function(){
-                    $(this).removeClass('in');
-                    $('[data-toggle="popover"]').popover('hide');
-                });
-                $('.popover-filter').addClass('in');
-            }).on('hide.bs.popover', function(){
-                $('.popover-filter').removeClass('in');
-            });
-
-        }
-    },
-    initCollapseArea: function(){
-        $('[data-toggle="pk-collapse"]').each(function () {
-            var thisdiv = $(this).attr("data-target");
-            $(thisdiv).addClass("pk-collapse");
-        });
-
-        $('[data-toggle="pk-collapse"]').hover(function(){
-            var thisdiv = $(this).attr("data-target");
-            if(!$(this).hasClass('state-open')){
-                $(this).addClass('state-hover');
-                $(thisdiv).css({
-                    'height':'30px'
-                });
-            }
-
-        },
-        function(){
-            var thisdiv = $(this).attr("data-target");
-            $(this).removeClass('state-hover');
-
-            if(!$(this).hasClass('state-open')){
-                $(thisdiv).css({
-                    'height':'0px'
-                });
-            }
-        }).click(function(event){
-                event.preventDefault();
-
-                var thisdiv = $(this).attr("data-target");
-                var height = $(thisdiv).children('.panel-body').height();
-
-                if($(this).hasClass('state-open')){
-                    $(thisdiv).css({
-                        'height':'0px',
-                    });
-                    $(this).removeClass('state-open');
-                } else {
-                    $(thisdiv).css({
-                        'height':height + 30,
-                    });
-                    $(this).addClass('state-open');
-                }
-            });
-    },
-    initSliders: function(){
-        // Sliders for demo purpose in refine cards section
-        if($('#sliderRegular').length != 0 ){
-            var rangeSlider = document.getElementById('sliderRegular');
-            noUiSlider.create(rangeSlider, {
-            	start: [ 5000 ],
-            	range: {
-            		'min': [  2000 ],
-            		'max': [ 10000 ]
-            	}
-            });
-        }
-        if($('#sliderDouble').length != 0){
-            var slider = document.getElementById('sliderDouble');
-            noUiSlider.create(slider, {
-            	start: [20, 80],
-            	connect: true,
-            	range: {
-            		'min': 0,
-            		'max': 100
-            	}
-            });
-        }
-    },
+  },
 
 }
 
@@ -255,6 +254,9 @@ examples = {
         marker.setMap(map);
         }
 }
+
+$(document).on('click', '.navbar-toggler', pk.toggleNavBar);
+$(document).on('click', '.nav-link', pk.toggleNavBar);
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
